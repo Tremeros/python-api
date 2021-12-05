aef03f0de6b71e6c1131408ac73747f175eee4bb
Pawe Tylczyski <pawel.tylczynski@gmail.com>
2021-12-05 21:35:07 +0100

    Creating user class

---
+++ app/core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                       PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+
+    def createUser(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
